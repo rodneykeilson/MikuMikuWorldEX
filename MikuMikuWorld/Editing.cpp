rodneykeilson/MikuMikuWorldEX

commit 28f0d465dc99d0b2cb1aa20b428758e022ed957d

Fix copying notes

MikuMikuWorld/Editing.cpp
@@ -111,7 +111,7 @@ namespace MikuMikuWorld
 		std::unordered_map<int, Note>& pasteNotes = flipPasting ? copyNotesFlip : copyNotes;
 		selectedNotes.clear();
 
-		for (auto&[id, note] : pasteNotes)
+		for (auto[id, note] : pasteNotes)
 		{
 			if (note.getType() == NoteType::Tap)
 			{
@@ -124,7 +124,7 @@ namespace MikuMikuWorld
 			}
 		}
 
-		for (auto&[id, hold] : copyHolds)
+		for (auto[id, hold] : copyHolds)
 		{
 			HoldNote hold = copyHolds[id];
 			int startID = nextID++;
@@ -148,7 +148,7 @@ namespace MikuMikuWorld
 			score.notes[startID] = start;
 			score.notes[endID] = end;
 
-			for (auto& step : hold.steps)
+			for (auto step : hold.steps)
 			{
 				Note mid = pasteNotes[step.ID];
 				mid.ID = nextID++;

commit 4eaed90157a1aed40096ef825c5c31ed8a313a76

Fix broken hold steps when pasting
Fix broken hold steps when pasting introduced by 28f0d46

MikuMikuWorld/Editing.cpp
@@ -148,7 +148,8 @@ namespace MikuMikuWorld
 			score.notes[startID] = start;
 			score.notes[endID] = end;
 
-			for (auto step : hold.steps)
+			// by reference here because we are modifying the steps of the newly created hold
+			for (auto &step : hold.steps)
 			{
 				Note mid = pasteNotes[step.ID];
 				mid.ID = nextID++;